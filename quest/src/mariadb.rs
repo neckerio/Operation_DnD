b9f85a1edeaff72bdf010460e798f69514e6bdbe
necker <github@necker.io>
2022-10-08 15:21:35 -0700

    Add: 1. gitignore 2. mysqlx to .toml 3. followed the docs more closely in mariadb.rs

--- quest/src/mariadb.rs
+++ quest/src/mariadb.rs
@@ -8,7 +8,7 @@ pub async fn connect_to_database () -> Result<(), sqlx::Error> {
         .max_connections(5)
         .connect("mysql://vagrant:vagrant@127.0.0.1/mariadb").await?;
 
-    let row: (i64,) = sqlx::query_as("SELECT ?")
+    let row: (i64,) = sqlx::query_as("SELECT $?")
         .bind(150_i64)
         .fetch_one(&pool).await?;
 
