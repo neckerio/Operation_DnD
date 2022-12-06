0e0eb20b6a8563cf5b7e4be6eee29216f1f1d692
necker <github@necker.io>
2022-12-05 16:32:54 -0800

    Deleted: almost everything. Just messing around

--- quest/src/main.rs
+++ quest/src/main.rs
@@ -1,16 +1,7 @@
 // Feeling things out
 // enum with variant values for class/race... make a impl method based on sub class/race?
 // use the match enum binding... somewhere. Maybe with the read_lines return?
-use mariadb::connect_to_database;
-use sqlx::Result;
-mod mariadb;
-
-enum Class {
-    Fighter,
-    Ranger,
-    Rogue,
-    Cleric,
-}
+use std::io;
 
 struct Character {
     name: String,
@@ -29,95 +20,77 @@ struct Character {
     charisma: i32,
 }
 
-impl Character {
-    fn armor_class(&self) -> i32 {
-        self.dexterity + 10
-    }
-    // NOTE: add conditional for class/race etc
-    fn hit_points(&self) -> i32 {
-        self.constitution + 10
+
+impl Default for Character {
+    fn default() -> Self {
+        Character {
+		name: String::from(""),
+		class: String::from(""),
+		race: String::from(""),
+		level: 0,
+		age: 0,
+		size: String::from(""),
+		height: String::from(""),
+		weight: 0,
+		strength: 0,
+		dexterity: 0,
+		constitution: 0,
+		intelligence: 0,
+		wisdom: 0,
+		charisma: 0,
+        }
     }
 }
 
-fn parse_args() -> Result<Character, String> {
-    let mut character = Character::default();
-    let mut fields = vec![
-        "Name",
-        "Class",
-        "Race",
-        "Level",
-        "Age",
-        "Size",
-        "Height",
-        "Weight",
-        "Strength",
-        "Dexterity",
-        "Constitution",
-        "Intelligence",
-        "Wisdom",
-        "Charisma"];
+// fn parse_args() -> Result<Character, String> {
+//     let mut character = Character::default;
+//     let mut fields = vec![
+//         "Name",
+//         "Class",
+//         "Race",
+//         "Level",
+//         "Age",
+//         "Size",
+//         "Height",
+//         "Weight",
+//         "Strength",
+//         "Dexterity",
+//         "Constitution",
+//         "Intelligence",
+//         "Wisdom",
+//         "Charisma"];
 
-    loop {
-        println!("Please enter your Character's Stats:");
-        for field in fields {
-            println!("{}:",  field);
-            let value = read!("{}\n");
-            match field {
-                "Name" => character.name = value,
-                "Class" => character.class = value,
-                "Race" => character.race = value,
-                "Level" => character.level = value,
-                "Age" => character.age = value,
-                "Size" => character.size = value,
-                "Height" => character.height = value,
-                "Weight" => character.weight = value,
-                "Strength" => character.strength = value,
-                "Dexterity" => character.dexterity = value,
-                "Constitution" => character.constitution = value,
-                "Intelligence" => character.intelligence = value,
-                "Wisdom" => character.wisdom = value,
-                "Charisma" => character.charisma = value,
-                _ => (),
-            }
-            fields.remove(field);
-        }
-        if field.is_empty() {
-            break;
-        }
-    }
-    Ok(character)
-}
-
-//Secret Message: my favorite number is 33.
-
-
+//     loop {
+//         println!("Please enter your Character's Stats:");
+//         for field in fields {
+//             println!("{}:",  field);
+//             let value = read!("{}\n");
+//             match field {
+//                 "Name" => character.name = value,
+//                 "Class" => character.class = value,
+//                 "Race" => character.race = value,
+//                 "Level" => character.level = value,
+//                 "Age" => character.age = value,
+//                 "Size" => character.size = value,
+//                 "Height" => character.height = value,
+//                 "Weight" => character.weight = value,
+//                 "Strength" => character.strength = value,
+//                 "Dexterity" => character.dexterity = value,
+//                 "Constitution" => character.constitution = value,
+//                 "Intelligence" => character.intelligence = value,
+//                 "Wisdom" => character.wisdom = value,
+//                 "Charisma" => character.charisma = value,
+//                 _ => (),
+//             }
+//             fields.remove(field);
+//         }
+//         if fields.is_empty() {
+//             break;
+//         }
+//     }
+//     Ok(character)
+// }
 
 
 fn main() {
-    let char1 = Character {
-        name: "Burn Battlehammer".to_string(),
-        class: "Fighter".to_string(),
-        race: "Dwarf".to_string(),
-        level: 1,
-        age: 175,
-        size: "Medium".to_string(),
-        height: "4'6".to_string(),
-        weight: 350,
-        strength: 3,
-        dexterity: 1,
-        constitution: 3,
-        intelligence: -1,
-        wisdom: 1,
-        charisma: 0,
-    };
-
-    print!("The character created is: {}\n", char1.name);
-    print!("They are a level {} {}\n", char1.level, char1.class);
-    print!(
-        "And as such, their armor class is {} and their HP are {}\n",
-        char1.armor_class(),
-        char1.hit_points()
-    );
-
-    connect_to_database();
 }
