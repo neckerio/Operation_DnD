9c4febd9e62662993eb684a29edb41fc7797e070
necker <github@necker.io>
2022-12-06 10:24:02 -0800

    Delete: Everything in main(). Starting over

--- quest/src/main.rs
+++ quest/src/main.rs
@@ -41,72 +41,8 @@ impl Default for Character {
 
 
 fn main() {
-    // Create a Vec to store Characters
-    let mut creations: Vec<Character> = Vec::new();
-
-    // Loop until the user enters "Done"
-    loop {
-	let mut character = Character::default();
-
-	// Create a Vec of field names that have not been set
-	let mut unset_fields = vec![
-	    "name",
-	    "class",
-	    "race",
-	    "level",
-	    "age",
-	    "size",
-	    "height",
-	    "weight",
-	    "strength",
-	    "dexterity",
-	    "constitution",
-	    "intelligence",
-	    "wisdom",
-	    "charisma",
-	];
-
-	// Loop until all fields have been set or Done and print fields which have not been set
-	loop {
-	    println!("Enter a Character Attribute followed by the associated Value\n");
-	    println!("Or 'done' when you are finished\n");
-	    println!("Unset Attributes:\n {}", unset_fields.join(","));
-
-	    let mut input = String::new();
-	    std::io::stdin().read_line(&mut input).expect("Failed to read input");
-
-	    let input = input.trim();
-
-	    if input == "done" {
-		break;
-	    }
-	    
-	    // Split the user's input into 'field' and 'value'
-	    let mut parts = input.splitn(2, " ");
-	    let field = parts.next().unwrap();
-	    let value = parts.next().unwrap();
-
-	    // Use a Match statement to set appropriate value to fields
-	    match field {
-		"name" => character.name = value.to_string(),
-		"class" => character.class = value.to_string(),
-		"race" => character.race = value.to_string(),
-		"level" => character.level = value.parse().expect("Failed to parse level"),
-		"age" => character.age = value.parse().expect("Failed to parse age"),
-		"size" => character.size = value.to_string(),
-		"height" => character.height = value.to_string(),
-		"weight" => character.weight = value.parse().expect("Failed to parse weight"),
-		"strength" => character.strength = value.parse().expect("Failed to parse strength"),
-		"dexterity" => character.dexterity = value.parse().expect("Failed to parse dexterity"),
-		"constitution" => character.constitution = value.parse().expect("Failed to parse constitution"),
-		"intelligence" => character.intelligence = value.parse().expect("Failed to parse intelligence"),
-		"wisdom" => character.wisdom = value.parse().expect("Failed to parse wisdom"),
-		"charisma" => character.charisma = value.parse().expect("Failed to parse charisma"),
-		_ => {}
-	    }
-
-	}
-    }
 
 }
+
+
 
