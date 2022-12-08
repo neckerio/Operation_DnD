0511e419643457ed5a456d731b32ce25189051a7
necker <github@necker.io>
2022-12-08 12:52:35 -0800

    Fixed: Changed the &str to &&str and it works now...

--- quest/src/main.rs
+++ quest/src/main.rs
@@ -1,20 +1,9 @@
 use std::io;
 
+
 struct Character {
     name: String,
-    class: String,
-    race: String,
     level: i32,
-    age: i32,
-    size: String,
-    height: String,
-    weight: i32,
-    strength: i32,
-    dexterity: i32,
-    constitution: i32,
-    intelligence: i32,
-    wisdom: i32,
-    charisma: i32,
 }
 
 
@@ -22,26 +11,71 @@ impl Default for Character {
     fn default() -> Self {
         Character {
 		name: String::from(""),
-		class: String::from(""),
-		race: String::from(""),
 		level: 0,
-		age: 0,
-		size: String::from(""),
-		height: String::from(""),
-		weight: 0,
-		strength: 0,
-		dexterity: 0,
-		constitution: 0,
-		intelligence: 0,
-		wisdom: 0,
-		charisma: 0,
         }
     }
 }
 
 
 fn main() {
+    // Use Default() to create a new instance
+    let mut character = Character::default();
 
+    // Create a vector of field names
+    let fields = vec![
+	"name",
+	"level",
+    ];
+
+    // Use a loop to continuously prompt the user for input
+    loop {
+	println!("Please enter the name of the field you want to set (or 'done' to exit):");
+
+	// Read the user's input from stdin
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).unwrap();
+
+	// Trim trailing white space from input
+	let input = input.trim();
+
+	// Check for "done" to break loop
+	if input == "done" {
+	    break;
+	}
+
+	// Otherwise check if the input is is a valid field name
+	if !fields.contains(&input) {
+	    // If field name is invalid print error message and continue the loop
+	    println!("'{}' is not a valid field name.", input);
+	    continue;
+	}
+
+	// If field name is valid prompt the user for a value
+	println!("Please enter the character's {}:", input);
+
+	// Read the user's input value
+	let mut value = String::new();
+	io::stdin().read_line(&mut value).unwrap();
+
+	// Trim the trailing white space from the value
+	let value = value.trim();
+
+	// Set field on the character instance
+	match input {
+	    "name" => character.name = value.to_owned(),
+	    "level" => character.level = value.parse().unwrap(),
+	    _ => (),
+	}
+
+	// Create a vector for the remaining fields
+	let remaining_fields: Vec<&&str> = fields
+	    .iter()
+	    .filter(|field| character.name == "" || character.level == 0)
+	    .collect();
+
+	// Print a message indicating the remaining fields to be filled
+	println!("Remaining fields: {:?}", remaining_fields);
+    }
 }
 
 
